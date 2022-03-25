4152ff40a9bbbc494599aeeae2d31923517fd3b1
AngeloDalo <angelo251158@gmail.com>
2022-03-25 11:13:14 +0100

    seeder

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\User;
 
 class UserSeeder extends Seeder
 {
@@ -9,8 +11,26 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $newUser = new User();
+        $newUser->username = 'Admin';
+        $newUser->name = 'Admin';
+        $newUser->date_of_birth = '1999-12-25';
+        $newUser->email = 'Admin@admin.it';
+        $string = '12345678';
+        $newUser->password = Hash::make($string);
+        $newUser->save();
+
+        for ($i = 0; $i < 20; $i++) {
+            $newUser = new User();
+            $newUser->username = $faker->userName();
+            $newUser->name = $faker->name();
+            $newUser->date_of_birth = $faker->date('Y-m-d');
+            $newUser->email = $faker->email();
+            $string = '123_FC76';
+            $newUser->password = Hash::make($string);
+            $newUser->save();
+        }
     }
 }
